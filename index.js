600c3528ac6354d6155aef7da7640c84a0111345
amitbed <amitbedarshi@gmail.com>
2016-10-25 16:15:48 +0300

    modifying parse-server-example to bitbiteserver

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'TUIzdlslPeWi8DnGk6ZRAcVRC6WapGxoSrU0nbAD',
+  masterKey: process.env.MASTER_KEY || '4h88jRWJbW7ifeWJl8Vrt4pO5Oq50S5S28TwVkLl', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://bitbiteserver.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -36,7 +36,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.status(200).send('(Test) I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
 // There will be a test page available on the /test path of your server url
